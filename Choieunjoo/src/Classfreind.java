3c6a33b2beef4e083e65a617805d89626f057c5c
EZEN-140 <EZEN-140@DESKTOP-1IKNAEC>
2022-07-13 15:54:22 +0900

    우리반 수강생분들 성함

---
+++ Choieunjoo/src/Classfreind.java
@@ -0,0 +1,18 @@
+
+public class Classfreind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("PSU   PSS    KCS");
+		System.out.println("KMJ   KSJ    KJS");
+		System.out.println("KTH   NGH    PYM");
+		System.out.println("SDY   YPG    LYB");
+		System.out.println("LJH   LDH    JYS");
+		System.out.println("JHS   HWJ    OGH");
+		
+	}
+
+}
